4608454e1f161ece6fed894f386c6a4d7ffa8b1d
Ankit Verma <syncing284@gmail.com>
2025-11-23 19:01:17 +0530

    install and setup better-auth in express

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,35 @@
 import express from "express"
 import dotenv from "dotenv"
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth.js";
+import cors from "cors";
 
 dotenv.config()
 
 const app = express()
 
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Replace with your frontend's origin
+    methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  })
+);
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
+
+app.use(express.json());
+
 app.use("/health", (req, res) => {
   res.send("Status: Working...")
 })
+
+app.get("/api/me", async (req, res) => {
+ 	const session = await auth.api.getSession({
+      headers: fromNodeHeaders(req.headers),
+    });
+	return res.json(session);
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Application started running on PORT ${process.env.PORT}`);
